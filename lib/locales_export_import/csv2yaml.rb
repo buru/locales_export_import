1f8f4051abe371edfa74a772b7410eb818f70724
Renato Nitta <renatocn@gmail.com>
2016-06-16 17:16:46 -0300

    add file prefix to be more flexible, remove condition, we can work without it

--- lib/locales_export_import/csv2yaml.rb
+++ lib/locales_export_import/csv2yaml.rb
@@ -5,7 +5,7 @@ module LocalesExportImport
   module Csv2Yaml
     extend self
 
-    def convert(input_file, output_path = nil)
+    def convert(input_file, output_path = nil, file_prefix = nil)
       @yaml = ::Hash.new
       ::CSV.foreach(::File.join(input_file), :headers => true) do |row|
         puts "inspect: #{row.inspect}"
@@ -26,7 +26,7 @@ module LocalesExportImport
       end
       puts "Resulting structure: #{@yaml.inspect}"
       @yaml.keys.each do |locale|
-        output_file = output_path ? ::File.join("#{output_path}/#{locale}.yml") : ::File.join("#{locale}.yml")
+        output_file = ::File.join("#{output_path}#{file_prefix}#{locale}.yml")
         ::File.write(output_file, @yaml[locale].to_yaml)
       end
     end
